8d5d1eac4af336e7934ec3fdd4cf3f1b2998c875
bsw012 <bsw012@bucknell.edu>
2015-06-07 20:38:51 -0400

    adding base maze

---
+++ Starter physics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float speed;
+		
+	private Rigidbody rb;
+		
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+		
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+			
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnCollisionStay(Collision collision)
+	{
+		Collider other = collision.collider;
+		if (other.gameObject.CompareTag ("Player"))
+		{	
+			print("YEAH!");
+		}
+	}
+}
